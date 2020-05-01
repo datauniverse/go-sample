7d7022a446e89bd08a33df5b6d58699cc78b7adc
Abhilash Anil <2020.abhilash@gmail.com>
2020-05-02 01:01:39 +0530

    Implementation of CSV File creation

--- cricket.go
+++ cricket.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
 	"gopkg.in/yaml.v2"
@@ -122,6 +124,28 @@ func main() {
 		log.Fatal(err)
 	}
 
+	games := [][]string{}
+	games = append(games, []string{
+		"GameId", "City", "StartDate", "Gender", "MatchType",
+		"MatchTypeNumber", "NeutralVenue",
+		"Winner", "Result", "OutcomeMethod", "Eliminator",
+		"OutcomeByInnings", "OutcomeByRuns",
+		"OutcomeByWickets", "OutcomeBowlOut",
+		"Overs", "PlayerOfMatch[0]", "TeamOne", "TeamTwo",
+		"TossDecision", "TossWinner", "UmpireOne", "UmpireTwo", "Venue",
+	})
+
+	deliveries := [][]string{}
+	deliveries = append(deliveries, []string{
+		"GameId", "Batsman", "Bowler", "NonStriker",
+		"RunsBatsman", "RunsExtras",
+		"RunsNonBoundary", "RunsTotal",
+		"ExtrasByes", "ExtrasLegByes",
+		"ExtrasNoBalls", "ExtrasPenalty",
+		"ExtrasWides",
+		"WicketKind", "WicketFielders", "WicketPlayerOut",
+	})
+
 	for _, f := range files {
 		if strings.HasSuffix(f.Name(), ".yaml") {
 			filename := fmt.Sprintf("%v%v", dirName, f.Name())
@@ -147,7 +171,81 @@ func main() {
 				break
 			}
 
-			// fmt.Println(g)
+			var pom, u1, u2, d, t1, t2 string
+			if len(g.Info.PlayerOfMatch) > 0 {
+				pom = g.Info.PlayerOfMatch[0]
+			}
+			if len(g.Info.Umpires) > 0 {
+				u1 = g.Info.Umpires[0]
+			} else if len(g.Info.Umpires) == 2 {
+				u2 = g.Info.Umpires[1]
+			}
+			if len(g.Info.Dates) > 0 {
+				d = g.Info.Dates[0]
+			}
+			if len(g.Info.Teams) > 0 {
+				t1 = g.Info.Teams[0]
+			} else if len(g.Info.Teams) == 2 {
+				t2 = g.Info.Teams[1]
+			}
+
+			game := []string{
+				strings.Split(f.Name(), ".")[0], g.Info.City, d, g.Info.Gender, g.Info.MatchType,
+				strconv.Itoa(int(g.Info.MatchTypeNumber)), strconv.Itoa(int(g.Info.NeutralVenue)),
+				g.Info.Outcome.Winner, g.Info.Outcome.Result, g.Info.Outcome.Method, g.Info.Outcome.Eliminator,
+				g.Info.Outcome.By.Innings, strconv.Itoa(int(g.Info.Outcome.By.Runs)),
+				strconv.Itoa(int(g.Info.Outcome.By.Wickets)), g.Info.Outcome.BowlOut,
+				strconv.Itoa(int(g.Info.Overs)), pom, t1, t2,
+				g.Info.Toss.Decision, g.Info.Toss.Winner, u1, u2, g.Info.Venue,
+			}
+			games = append(games, game)
+
+			for _, i := range g.Innings {
+				for _, in := range i {
+					for _, d := range in.Deliveries {
+						for _, del := range d {
+							var wf string
+							if len(del.Wicket.Fielders) > 0 {
+								wf = del.Wicket.Fielders[0]
+							}
+
+							delivery := []string{
+								strings.Split(f.Name(), ".")[0], del.Batsman, del.Bowler, del.NonStriker,
+								strconv.Itoa(int(del.Runs.Batsman)), strconv.Itoa(int(del.Runs.Extras)),
+								strconv.Itoa(int(del.Runs.NonBoundary)), strconv.Itoa(int(del.Runs.Total)),
+								strconv.Itoa(del.Extras.Byes), strconv.Itoa(del.Extras.LegByes),
+								strconv.Itoa(del.Extras.NoBalls), strconv.Itoa(del.Extras.Penalty),
+								strconv.Itoa(del.Extras.Wides),
+								del.Wicket.Kind, wf, del.Wicket.PlayerOut,
+							}
+
+							deliveries = append(deliveries, delivery)
+						}
+					}
+				}
+			}
+
+			writeCSV("data/games.csv", games)
+			writeCSV("data/deliveries.csv", deliveries)
+		}
+	}
+
+}
+
+func writeCSV(filename string, data [][]string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, d := range data {
+		err := writer.Write(d)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 }
